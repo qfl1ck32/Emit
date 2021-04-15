4819c09fa5dad4f9425146b1a71ec76bd0baad9f
qfl1ck32 <qfl1ck32@yahoo.com>
2021-04-15 20:30:03 +0300

    Create MySQL query function

--- backEnd/server.js
+++ backEnd/server.js
@@ -7,6 +7,21 @@ dotenv.config()
 
 const connection = mysql.createConnection(process.env.DATABASE_URL)
 
+const sendQuery = async (query, parametersToBind) => {
+    return await new Promise((resolve, reject) => {
+        connection.query(query, parametersToBind, (err, results, fields) => {
+            if (err)
+                return reject(err)
+            
+            const answer = results.map(rowDataPacket => {
+                return Object.assign({}, rowDataPacket)
+            })
+
+            return resolve(answer)
+        })
+    })
+}
+
 const 
     app = express(),
     port = 8080
