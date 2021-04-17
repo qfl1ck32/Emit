72530da386ccfc61b8705e0a1d290dfb6e4d27cf
qfl1ck32 <qfl1ck32@yahoo.com>
2021-04-17 20:24:26 +0300

    Create singleton for mapping password to its "strength"
    Only accessible function is getStrength(password), which will return an object of the form { progress, color, message }.

---
+++ frontEnd/src/helpers/PasswordStrengthChecker.tsx
@@ -0,0 +1,94 @@
+export default class PasswordStrengthChecker {
+    private static instance: PasswordStrengthChecker
+
+    private progressToColorAndMessage: object
+
+    private oneLowerCase: RegExp
+    private oneUpperCase: RegExp
+    private oneNumber: RegExp
+    private oneSpecialCharacter: RegExp
+    private atLeast8Characters: RegExp
+
+    private check = (regexp: RegExp, text: string): number => {
+        return regexp.test(text) ? 1 : 0
+    }
+
+    private checkAll = (text: string): number => {
+        return [this.oneLowerCase,
+                this.oneUpperCase,
+                this.oneNumber,
+                this.oneSpecialCharacter,
+                this.atLeast8Characters].reduce((acc: number, curr: RegExp) => {
+                    return acc + 0.2 * this.check(curr, text)
+                }, 0)
+    }
+
+    public getStrength(password: string): object {
+        const progress = this.checkAll(password)
+
+        for (let index in this.progressToColorAndMessage) {
+            const currentProgress: number = parseInt(index) / 100
+
+            if (currentProgress >= progress)
+                return {
+                    ...this.progressToColorAndMessage[index],
+                    progress: currentProgress
+                }
+        }
+
+        return []
+    }
+
+    private constructor() {
+        this.progressToColorAndMessage = {}
+
+        this.oneLowerCase = new RegExp('^(?=.*[a-z])')
+        this.oneUpperCase = new RegExp('^(?=.*[A-Z])')
+        this.oneNumber = new RegExp('^(?=.*[0-9])')
+        this.oneSpecialCharacter = new RegExp('(?=.*[!@#$%^&*,.()])')
+        this.atLeast8Characters = new RegExp('(?=.{8,})')
+
+        this.progressToColorAndMessage = {
+            0: {
+                color: 'lightgreen',
+                message: ''
+            },
+
+            20: {
+                color: 'red',
+                message: 'Weak'
+            },
+            
+            40: {
+                color: 'orange',
+                message: 'Medium'
+            },
+
+            60: {
+                color: 'orange',
+                message: 'Medium'
+            },
+
+            80: {
+                color: 'green',
+                message: 'Pretty good'
+            },
+
+            100: {
+                color: 'green',
+                message: 'Strong'
+            }
+        }
+    }
+
+    
+
+    public static getInstance(): PasswordStrengthChecker {
+        if (!PasswordStrengthChecker.instance)
+            PasswordStrengthChecker.instance = new PasswordStrengthChecker()
+
+        return PasswordStrengthChecker.instance;
+    }
+
+
+}
