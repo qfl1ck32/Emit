3def55b78819321590c31a85d3f3a97a606c1573
qfl1ck32 <qfl1ck32@yahoo.com>
2021-04-19 08:12:05 +0300

    Create singleton for Redis Client

---
+++ backEnd/RedisConnection.ts
@@ -0,0 +1,24 @@
+import redis from 'redis'
+
+class RedisConnection {
+    private client: redis.RedisClient;
+
+    public static instance: RedisConnection;
+
+    public static getInstance(): RedisConnection {
+        if (RedisConnection.instance == null)
+            RedisConnection.instance = new RedisConnection()
+
+        return RedisConnection.instance;
+    }
+
+    private constructor() {
+        this.client = redis.createClient(6379)
+    }
+
+    public getClient(): redis.RedisClient {
+        return this.client;
+    }
+}
+
+export default RedisConnection
